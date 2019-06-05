748e07922a8fb1c94c49b084a1f0e17c31d81bc5
Renpet-hhh <renandcsc@gmail.com>
2019-06-05 10:49:55 -0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# pomodoro-clock
-FCC Project: React Pomodoro Clock
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomodoro-clock/precache-manifest.85e6cb2c0c2684402279135a69dee6f5.js"
+  "/precache-manifest.70930247e0d05baa07a4a1d5e6cde443.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pomodoro-clock/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
